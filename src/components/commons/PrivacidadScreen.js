f1abdb977c0cc968106a04fb2348d4789e17c2c8
Jorge Reyes <rjorge99@hotmail.com>
2021-12-31 12:56:21 -0600

    Se agrega politica de privacidad

---
+++ src/components/commons/PrivacidadScreen.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PrivacidadScreen = () => {
+    return <div>Politica de privacidad</div>;
+};
--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { AuthScreen } from "../components/auth/AuthScreen";
 import { LoginScreen } from "../components/auth/LoginScreen";
 import { RegisterScreen } from "../components/auth/RegisterScreen";
+import { PrivacidadScreen } from "../components/commons/PrivacidadScreen";
 
 export const AuthRouter = () => {
     return (
@@ -11,6 +12,7 @@ export const AuthRouter = () => {
                 <Route path="login" element={<LoginScreen />} />
                 <Route path="register" element={<RegisterScreen />} />
             </Route>
+            <Route path="/politicas" element={<PrivacidadScreen />} />
         </Routes>
     );
 };
