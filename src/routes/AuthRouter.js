a51f7c78a780eb7cf12cd8f6d0de361aed676896
Jorge Reyes <rjorge99@hotmail.com>
2022-01-02 22:03:09 -0600

    Se descomenta infrmacion

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { AuthScreen } from "../components/auth/AuthScreen";
+import { LoginScreen } from "../components/auth/LoginScreen";
 import { RegisterScreen } from "../components/auth/RegisterScreen";
 import { PrivacidadScreen } from "../components/commons/PrivacidadScreen";
 
@@ -8,6 +9,7 @@ export const AuthRouter = () => {
     return (
         <Routes>
             <Route path="/auth" element={<AuthScreen />}>
+                <Route path="login" element={<LoginScreen />} />
                 <Route path="register" element={<RegisterScreen />} />
             </Route>
             <Route path="/politicas" element={<PrivacidadScreen />} />
